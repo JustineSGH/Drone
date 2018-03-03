308a90964aa7327fd4920b57584ae48552427b7a
JustineSGH <justine.sgherzi@etud.u-picardie.fr>
2018-03-03 12:54:07 +0100

    Ajout de Google Map

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -3,6 +3,7 @@ import './Body.css';
 
 import ChartPH from '../charts/ChartPH.js';
 import ChartTemperature from '../charts/ChartTemperature';
+import MapContainer from '../map/MapContainer';
 
 class Body extends Component {
     render(){
@@ -15,72 +16,6 @@ class Body extends Component {
                         </li>
                         <li className="breadcrumb-item active">Mon Tableau de bord</li>
                     </ol>
-                    <div className="row">
-                        <div className="col-xl-3 col-sm-6 mb-3">
-                            <div className="card text-white bg-primary o-hidden h-100">
-                                <div className="card-body">
-                                    <div className="card-body-icon">
-                                        <i className="fa fa-fw fa-comments"></i>
-                                    </div>
-                                    <div className="mr-5">Démarrer les mesures</div>
-                                </div>
-                                <a className="card-footer text-white clearfix small z-1" href="#">
-                                <span className="float-left">View Details</span>
-                                <span className="float-right">
-                                    <i className="fa fa-angle-right"></i>
-                                </span>
-                                </a>
-                            </div>
-                        </div>
-                        <div className="col-xl-3 col-sm-6 mb-3">
-                            <div className="card text-white bg-warning o-hidden h-100">
-                                <div className="card-body">
-                                <div className="card-body-icon">
-                                    <i className="fa fa-fw fa-list"></i>
-                                </div>
-                                <div className="mr-5">11 New Tasks!</div>
-                                </div>
-                                <a className="card-footer text-white clearfix small z-1" href="#">
-                                <span className="float-left">View Details</span>
-                                <span className="float-right">
-                                    <i className="fa fa-angle-right"></i>
-                                </span>
-                                </a>
-                            </div>
-                        </div>
-                        <div className="col-xl-3 col-sm-6 mb-3">
-                            <div className="card text-white bg-success o-hidden h-100">
-                                <div className="card-body">
-                                <div className="card-body-icon">
-                                    <i className="fa fa-fw fa-shopping-cart"></i>
-                                </div>
-                                <div className="mr-5">123 New Orders!</div>
-                                </div>
-                                <a className="card-footer text-white clearfix small z-1" href="#">
-                                <span className="float-left">View Details</span>
-                                <span className="float-right">
-                                    <i className="fa fa-angle-right"></i>
-                                </span>
-                                </a>
-                            </div>
-                        </div>
-                        <div className="col-xl-3 col-sm-6 mb-3">
-                            <div className="card text-white bg-danger o-hidden h-100">
-                                <div className="card-body">
-                                <div className="card-body-icon">
-                                    <i className="fa fa-fw fa-support"></i>
-                                </div>
-                                <div className="mr-5">13 New Tickets!</div>
-                                </div>
-                                <a className="card-footer text-white clearfix small z-1" href="#">
-                                <span className="float-left">View Details</span>
-                                <span className="float-right">
-                                    <i className="fa fa-angle-right"></i>
-                                </span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
                     <div className="card mb-3">
                         <div className="card-header">
                         <i className="fa fa-area-chart"></i> Acidité de l'eau et oxygène</div>
@@ -101,13 +36,13 @@ class Body extends Component {
                                         </div>
                                         <div className="col-sm-4 text-center my-auto">
                                             <div className="h4 mb-0 text-danger">11.0 °C</div>
-                                            <div className="small text-muted">Température plus haute</div>
+                                            <div className="small text-muted">Maximale</div>
                                             <hr/>
                                             <div className="h4 mb-0 text-success">6.7 °C</div>
                                             <div className="small text-muted">Température moyenne</div>
                                             <hr/>
                                             <div className="h4 mb-0 text-primary">-0.5 °C</div>
-                                            <div className="small text-muted">Température plus basse</div>
+                                            <div className="small text-muted">Minimale</div>
                                         </div>
                                     </div>
                                 </div>
@@ -115,15 +50,6 @@ class Body extends Component {
                             </div>
                         </div>
                         <div className="col-lg-4">
-                            <div className="card mb-3">
-                                <div className="card-header">
-                                <i className="fa fa-pie-chart"></i>Oxygène</div>
-                                <div className="card-body">
-                                    <canvas id="myPieChart" width="100%" height="100"></canvas>
-                                </div>
-                                <div className="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
-                            </div>
-                    
                             <div className="card mb-3">
                                 <div className="card-header">
                                 <i className="fa fa-bell-o"></i> Feed Example</div>
@@ -170,6 +96,85 @@ class Body extends Component {
                                     </div>
                                 </a>
                                 <a className="list-group-item list-group-item-action" href="#">View all activity...</a>
+                                </div>
+                                <div className="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div className="row">
+                        <div className="col-xl-3 col-sm-6 mb-3">
+                            <div className="card text-white bg-primary o-hidden h-100">
+                                <div className="card-body">
+                                    <div className="card-body-icon">
+                                        <i className="fa fa-fw fa-info-circle"></i>
+                                    </div>
+                                    <div className="mr-5">Informations capteurs</div>
+                                </div>
+                                <a className="card-footer text-white clearfix small z-1" href="#">
+                                <span className="float-left">Plus de détails</span>
+                                <span className="float-right">
+                                    <i className="fa fa-angle-right"></i>
+                                </span>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="col-xl-3 col-sm-6 mb-3">
+                            <div className="card text-white bg-warning o-hidden h-100">
+                                <div className="card-body">
+                                <div className="card-body-icon">
+                                    <i className="fa fa-fw fa-list"></i>
+                                </div>
+                                <div className="mr-5">Information trames</div>
+                                </div>
+                                <a className="card-footer text-white clearfix small z-1" href="#">
+                                <span className="float-left">Plus de détails</span>
+                                <span className="float-right">
+                                    <i className="fa fa-angle-right"></i>
+                                </span>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="col-xl-3 col-sm-6 mb-3">
+                            <div className="card text-white bg-success o-hidden h-100">
+                                <div className="card-body">
+                                <div className="card-body-icon">
+                                    <i className="fa fa-fw fa-table"></i>
+                                </div>
+                                <div className="mr-5">Infomation buffer</div>
+                                </div>
+                                <a className="card-footer text-white clearfix small z-1" href="#">
+                                <span className="float-left">Plus de détails</span>
+                                <span className="float-right">
+                                    <i className="fa fa-angle-right"></i>
+                                </span>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="col-xl-3 col-sm-6 mb-3">
+                            <div className="card text-white bg-danger o-hidden h-100">
+                                <div className="card-body">
+                                <div className="card-body-icon">
+                                    <i className="fa fa-fw fa-exclamation-circle"></i>
+                                </div>
+                                <div className="mr-5">Infomation système</div>
+                                </div>
+                                <a className="card-footer text-white clearfix small z-1" href="#">
+                                <span className="float-left">Plus de détails</span>
+                                <span className="float-right">
+                                    <i className="fa fa-angle-right"></i>
+                                </span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="card mb-3">
+                                <div className="card-header">
+                                <i className="fa fa-globe"></i>GPS</div>
+                                <div className="card-body-map">
+                                    <MapContainer></MapContainer>
                                 </div>
                                 <div className="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
                             </div>
---
+++ src/components/map/MapContainer.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+ 
+export class MapContainer extends Component {
+render() {
+    const style = {
+        width: '100%',
+        height: '100%'
+    }
+    return (
+        <Map google={this.props.google}
+            style={{width: '100%', height: '100%', position: 'relative'}}
+            className={'map'}
+            zoom={14}>
+        <Marker
+            title={'The marker`s title will appear as a tooltip.'}
+            name={'SOMA'}
+            position={{lat: 37.778519, lng: -122.405640}} />
+        <Marker
+            name={'Dolores park'}
+            position={{lat: 37.759703, lng: -122.428093}} />
+        <Marker />
+        <Marker
+            name={'Your position'}
+            position={{lat: 37.762391, lng: -122.439192}}
+            />
+        </Map>
+    );
+    }
+}
+export default GoogleApiWrapper({
+    apiKey: ('AIzaSyDwBz5Nm0umXV35mcd3lyaM1D6R2xhvn_w')
+})(MapContainer)
